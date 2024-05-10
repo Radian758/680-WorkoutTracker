8f3c3dad25719e0d4b99d068a004029a724243a4
Adrian Vasquez <adrianvasquez@Adrians-Air.attlocal.net>
2024-05-10 12:39:07 -0700

    git init

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Adrian Vasquez on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
