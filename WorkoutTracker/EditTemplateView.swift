a8cdf11bb39df16a68c89e12f3963c795d070808
Adrian Vasquez <adrianvasquez@Adrians-Air.attlocal.net>
2024-05-13 13:15:24 -0700

    init workoutname

--- WorkoutTracker/EditTemplateView.swift
+++ WorkoutTracker/EditTemplateView.swift
@@ -9,7 +9,7 @@ import FirebaseAuth
 struct EditTemplateView: View {
     @State var workoutName: String = ""
     @State var exercises: [Exercise]// if workout is not nil, assign it workout.exercises
-//    @StateObject var viewModel = ContentViewModel()
+    //    @StateObject var viewModel = ContentViewModel()
     let workout: Workout? // if workout is nil, create empty workout template
     @Binding var isEditViewFromPlusPresented: Bool
     
@@ -17,6 +17,7 @@ struct EditTemplateView: View {
         self.workout = workout
         self._isEditViewFromPlusPresented = editViewFromPlusPresented
         if let workout = workout {
+            self._workoutName = State(initialValue: workout.name)
             print("workout is not nil!")
         } else {
             print("workout is nil: \(workout)")
@@ -30,11 +31,11 @@ struct EditTemplateView: View {
             Spacer()
             Button(action: {
                 // Save the workout template
-//                saveWorkout()
+                //                saveWorkout()
                 print("Exited EditTemplateView")
-//              Disable the fullScreenCover by setting isEditViewPresented to true
+                //              Disable the fullScreenCover by setting isEditViewPresented to true
                 isEditViewFromPlusPresented = false
-
+                
                 //                WorkoutHistoryView(userId: viewModel.currentUserId)
                 
             }, label: {
@@ -61,73 +62,64 @@ struct EditTemplateView: View {
                     ExerciseView(exercise: $exercises[index])
                 }
             }
-//            .onAppear {
-//                 // Ensure workout and its exercises are not nil
-//                 if let workout = workout {
-//                     print("workout is not nil!")
-//                     exercises = workout.exercises
-//                 } else {
-//                     print("workout is nil: \(workout)")
-//                 }
-//             }
             .padding()
         }
     }
     
     func saveWorkout() {
-            if let currentUser = Auth.auth().currentUser {
-                let userID = currentUser.uid
-                
-                guard !workoutName.isEmpty else {
-                    print("Workout name is empty")
-                    return
-                }
-                
-                let db = Firestore.firestore()
-                var workoutData: [String: Any] = [
-                    "name": workoutName,
-                    // Convert exercises to Firestore-compatible data
-                    "exercises": exercises.map { $0.asDictionary() }
-                ]
-                
-                if let workout = workout {
-                    // Update existing workout
-                    db.collection("users")
-                        .document(userID)
-                        .collection("workouts")
-                        .document(workout.id.uuidString)
-                        .setData(workoutData) { error in
+        if let currentUser = Auth.auth().currentUser {
+            let userID = currentUser.uid
+            
+            guard !workoutName.isEmpty else {
+                print("Workout name is empty")
+                return
+            }
+            
+            let db = Firestore.firestore()
+            var workoutData: [String: Any] = [
+                "name": workoutName,
+                // Convert exercises to Firestore-compatible data
+                "exercises": exercises.map { $0.asDictionary() }
+            ]
+            
+            if let workout = workout {
+                // Update existing workout
+                db.collection("users")
+                    .document(userID)
+                    .collection("workouts")
+                    .document(workout.id.uuidString)
+                    .setData(workoutData) { error in
                         if let error = error {
                             print("Error updating workout: \(error.localizedDescription)")
                         } else {
                             print("Workout updated successfully")
                         }
                     }
-                } else {
-                    // Create new workout
-                    db.collection("users")
-                        .document(userID)
-                        .collection("workouts")
-                        .addDocument(data: workoutData) { error in
+            } else {
+                // Create new workout
+                db.collection("users")
+                    .document(userID)
+                    .collection("workouts")
+                    .addDocument(data: workoutData) { error in
                         if let error = error {
                             print("Error adding workout: \(error.localizedDescription)")
                         } else {
                             print("Workout added successfully")
                         }
                     }
-                }
-            } else {
-                print("No user is currently signed in")
             }
-
-        } // saveWorkout()
+        } else {
+            print("No user is currently signed in")
+        }
+        
+    } // saveWorkout()
     
 }
 
-//#Preview {
-//    EditTemplateView(workout: nil, editViewPresented: Binding(get: {
-//        return true
-//    }, set: { _ in
-//        
-//    }))
-//}
+#Preview {
+    EditTemplateView(workout: nil, editViewFromPlusPresented: Binding(get: {
+        return true
+    }, set: { _ in
+        
+    }))
+}
